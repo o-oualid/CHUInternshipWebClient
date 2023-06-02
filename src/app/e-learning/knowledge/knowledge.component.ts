30569066be09c43e7af838e4c1adc2bcc4b89c6b
ouazrou oualid <o-oualid@outlook.com>
2023-06-02 17:12:14 +0100

    shuffle knowledge list

--- src/app/e-learning/knowledge/knowledge.component.ts
+++ src/app/e-learning/knowledge/knowledge.component.ts
@@ -20,11 +20,11 @@ export class KnowledgeComponent {
 
 
   ngOnInit(): void {
-    this.quizzes.push({classification: "Class1", image: "assets/OIP.jpeg", notes: ""});
-    this.quizzes.push({classification: "Class2", image: "assets/R.jpeg", notes: ""});
     this.quizzes.push({classification: "Class3", image: "assets/R (1).jpeg", notes: ""});
+    this.quizzes.push({classification: "Class2", image: "assets/R.jpeg", notes: ""});
     this.quizzes.push({classification: "Class4", image: "assets/test.jpeg", notes: ""});
     this.quizzes.push({classification: "None", image: "assets/test0.jpg", notes: ""});
+    this.quizzes.push({classification: "Class1", image: "assets/OIP.jpeg", notes: ""});
   }
 
   nextQuiz() {
